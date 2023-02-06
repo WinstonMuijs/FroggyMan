1e0c2de0a9c09c532a51f2aae72fb2e796cd7a07
Winston Muijs <winstonhmuijs@gmail.com>
2023-02-06 15:51:58 +0100

    Updates the images, adn movement directions

--- player.py
+++ player.py
@@ -5,19 +5,16 @@ from os import walk
 class Player(pygame.sprite.Sprite):
     def __init__(self, group, pos):
         super().__init__(group)
+        self.status = 'right'
         self.index = 0
-        # self.animation =
-        self.import_frogman()
-        self.image = self.animation[self.index]
+        self.animations = self.import_frogman()
+        self.image = self.animations[self.status][self.index]
         self.rect = self.image.get_rect(center=pos)
         self.pos = pygame.math.Vector2(self.rect.center)
         self.direction = pygame.math.Vector2()
         self.speed = 200
 
     def import_frogman(self):
-        path = "./graphics/player/right/"
-        self.animation = [pygame.image.load(f'{path}{frogman}.png').convert_alpha() for frogman in range(4)]
-        # return self.animation
 
         # animations
         self.animations = {}
@@ -33,7 +30,6 @@ class Player(pygame.sprite.Sprite):
                     self.animations[key].append(surf)
         return self.animations
 
-
     def movement(self, dt):
         # normalize the length vector for diagonal direction speed
         if self.direction.magnitude() != 0:
@@ -43,36 +39,38 @@ class Player(pygame.sprite.Sprite):
         self.rect.center = (round(self.pos.x), round(self.pos.y))
 
     def animation_player(self, dt):
-        self.index += 10 * dt
-        if self.index < len(self.animation):
-            self.image = self.animation[int(self.index)]
+        current_animation = self.animations[self.status]
+        if self.direction.magnitude() != 0:
+            self.index += 10 * dt
+            if self.index >= len(current_animation):
+                self.index = 0
+
         else:
             self.index = 0
+        self.image = current_animation[self.index]
 
     def keyboard_input(self, dt):
         keys = pygame.key.get_pressed()
+
         if keys[pygame.K_RIGHT]:
+            self.status = 'right'
             self.direction.x = 1
-            self.animation_player(dt)
         elif keys[pygame.K_LEFT]:
+            self.status = "left"
             self.direction.x = -1
-            self.image = pygame.image.load("./graphics/player/left/0.png")
         else:
             self.direction.x = 0
-            # if self.rect.x > WINDOW_WIDTH - 48:
-            #     self.rect.x = WINDOW_WIDTH - 48
 
         if keys[pygame.K_UP]:
+            self.status = "up"
             self.direction.y = -1
-            self.image = pygame.image.load("./graphics/player/up/0.png")
+
         elif keys[pygame.K_DOWN]:
+            self.status = 'down'
             self.direction.y = 1
-            self.image = pygame.image.load("./graphics/player/down/0.png")
+
         else:
             self.direction.y = 0
-
-            # if self.rect.y > WINDOW_HEIGHT - 64:
-            #     self.rect.y = WINDOW_HEIGHT - 64
 
     def update(self, dt):
         self.animation_player(dt)
